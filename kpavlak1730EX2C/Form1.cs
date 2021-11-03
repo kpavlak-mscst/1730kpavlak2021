d911d27f9655072bc89f9e98785293afcee80a0c
kpavlak <vg4534ev.go.minnstate.edu>
2021-11-03 12:41:25 -0500

    completed EX2E

---
+++ kpavlak1730EX2C/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kpavlak1730EX2C
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void setMinButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void setMaxButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
