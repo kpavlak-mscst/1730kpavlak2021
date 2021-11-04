86112155815002ae7d44fafc3db5b9d878724b92
kpavlak <vg4534ev.go.minnstate.edu>
2021-11-03 22:32:10 -0500

    completedEX2EunitTest

--- kpavlak1730EX2E/Form1.cs
+++ kpavlak1730EX2E/Form1.cs
@@ -38,85 +38,89 @@ namespace kpavlak1730EX2E
 
 
             // #01
-
             decimal subtotal = Convert.ToDecimal(input01aTextBox.Text);
-            result01TextBox.Text = (subtotal >= 250 && subtotal < 500).ToString();
+            //result01TextBox.Text = (subtotal >= 250 && subtotal < 500).ToString();
+            result01TextBox.Text = (LogicalOperations.q01(subtotal)).ToString();
 
             // #02
-
             int timeInService = Convert.ToInt32(input02aTextBox.Text);
-            result02TextBox.Text = (timeInService <= 4 || timeInService >= 12).ToString();
+            //result02TextBox.Text = (timeInService <= 4 || timeInService >= 12).ToString();
+            result02TextBox.Text = (
+            LogicalOperations.q02(timeInService)
+            ).ToString();
 
-            // #03-06
-
+            // #03 - 06 initialization
             bool isValid = Convert.ToBoolean(input03aTextBox.Text);
             int years = Convert.ToInt32(input03cTextBox.Text);
 
-
             // #03
-
             int counter = Convert.ToInt32(input03bTextBox.Text);
-            result03TextBox.Text = (isValid == true && counter++ < years).ToString();
+            result03TextBox.Text = (LogicalOperations.q03(isValid, years, counter)
+            ).ToString();
             result03bTextBox.Text = counter.ToString();
 
             // #04
 
             counter = Convert.ToInt32(input03bTextBox.Text);
-            result04TextBox.Text = (isValid == true & counter ++ < years).ToString();
-            result04bTextBox.Text = counter.ToString();
-
-            // #04
-
-            counter = Convert.ToInt32(input03bTextBox.Text);
-            result04TextBox.Text = (isValid == true & counter++ < years).ToString();
+            result04TextBox.Text =( 
+            LogicalOperations.q04(isValid, counter, years)
+            LogicalOperations.q04(isValid, years, counter)
+                ).ToString();
             result04bTextBox.Text = counter.ToString();
 
             // #05
 
             counter = Convert.ToInt32(input03bTextBox.Text);
-            result05TextBox.Text = (isValid == true || counter++ < years).ToString();
+            result05TextBox.Text = (
+            LogicalOperations.q05(isValid, counter, years)
+            LogicalOperations.q05(isValid, years, counter)
+            ).ToString();
             result05bTextBox.Text = counter.ToString();
 
             // #06
-
             counter = Convert.ToInt32(input03bTextBox.Text);
-            result06TextBox.Text = (isValid == true | counter++ < years).ToString();
+            result06TextBox.Text = (
+            LogicalOperations.q06(isValid, counter, years)
+            LogicalOperations.q06(isValid, years, counter)
+            ).ToString();
             result06bTextBox.Text = counter.ToString();
 
             // #07
-
             DateTime startDate = Convert.ToDateTime(input07aTextBox.Text);
             DateTime experationDate = Convert.ToDateTime(input07bTextBox.Text);
             DateTime date = Convert.ToDateTime(input07cTextBox.Text);
             isValid = Convert.ToBoolean(input07dTextBox.Text);
-            result07TextBox.Text = (date > startDate && date < experationDate || isValid == true
+            result07TextBox.Text = (
+            LogicalOperations.q07(isValid, date, startDate, experationDate)
+            LogicalOperations.q07(startDate, experationDate, date, isValid)
             ).ToString();
 
             // #08
-
             decimal thisYTD = Convert.ToDecimal(input08aTextBox.Text);
             decimal lastYTD = Convert.ToDecimal(input08bTextBox.Text);
             string empType = input08cTextBox.Text;
             int startYear = Convert.ToInt32(input08dTextBox.Text);
             int currentYear = Convert.ToInt32(input08eTextBox.Text);
-            result08TextBox.Text = (((thisYTD > lastYTD) || empType == "Part time") 
-            && startYear < currentYear).ToString();
+            result08TextBox.Text = (
+            LogicalOperations.q08(thisYTD, lastYTD, empType, startYear, currentYear)
+            ).ToString();
 
             // #09
-
             counter = Convert.ToInt32(input09aTextBox.Text);
             years = Convert.ToInt32(input09bTextBox.Text);
-            result09TextBox.Text = (!(counter++ >= years)).ToString();
+            result09TextBox.Text = (
+            LogicalOperations.q09(counter, years)
+            ).ToString();
             result09bTextBox.Text = counter.ToString();
 
-            //#10
-
+            // #10
             int a = Convert.ToInt32(input10aTextBox.Text);
             int b = Convert.ToInt32(input10bTextBox.Text);
             int c = Convert.ToInt32(input10cTextBox.Text);
             int d = Convert.ToInt32(input10dTextBox.Text);
-
-            result10TextBox.Text = (a > b && b < c || c < d).ToString();
+            result10TextBox.Text = (
+           LogicalOperations.q10(a, b, c, d)
+            ).ToString();
 
             //bool v = a > b;
             //bool w = b < c;
@@ -124,7 +128,6 @@ namespace kpavlak1730EX2E
             //bool y = v && w;
             //bool z = y || x;
             //result10TextBox.Text = z.ToString();
-
 
 
         }
---
+++ kpavlak1730EX2EUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("kpavlak1730EX2EUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("kpavlak1730EX2EUnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("90893f45-f244-412e-9adb-206a22268b25")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
