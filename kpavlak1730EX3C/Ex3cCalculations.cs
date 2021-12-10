6d13233a48103ddab0d93de2b3a6db5291d6b09f
kpavlak <vg4534ev.go.minnstate.edu>
2021-12-09 18:00:22 -0600

    Completed EX3C

---
+++ kpavlak1730EX3C/Ex3cCalculations.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kpavlak1730EX3C
+{
+    public class Ex3cCalculations
+    {
+        public static string Calc0(int index)
+        {
+            string result = "Invalid index";
+            string status = "Invalid index";
+
+            try
+            {
+                string[] days = new string[] { "", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+
+                string day = days[index];
+
+                if (index >= 1 && index < 8)
+
+                    return day.ToString();
+
+                else return status.ToString();
+            }
+            catch
+            { 
+
+            }
+            return result;
+        }
+
+        public static string Calc1(string search)
+        {
+            search = search.Trim();
+            search = search.ToUpper();
+            string result = "Invalid input";
+            try
+            {
+                string[] days = { "SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY" };
+                string[] hours = { "Closed", "10am - 6pm", "10am - 6pm", "10am - 6pm", "10am - 9pm", "10am - 6pm", "8am - 4pm" };
+
+                int index = Array.IndexOf(days, search);
+                string day = hours[index];
+                return day.ToString();
+            }
+            catch
+            {
+
+            }
+            return result;
+        }
+
+        public static int Calc2(int[] numbers)
+        {
+            int sum = 0;
+            foreach (int digit in numbers)
+                sum += digit;
+            return sum;
+        }
+        public static double Calc3(double[] numbers, int digit)
+        {
+            double sum = 0.0;
+            int i = 0;
+
+            if (digit > 0 && digit <= numbers.GetLength(0))
+            {
+                for (i = 0; i < digit; i++)
+                sum += numbers[i];
+                return sum;
+            }
+
+            else return 0.0;
+        }
+        public static double Calc5(double[] numbers)
+        {
+            double sum = 0.0;
+            int length = numbers.Length;
+
+            if (length > 0)
+            {
+                foreach (double number in numbers)
+                sum += number;
+                double average = sum / numbers.Length;
+                return average;
+            }
+
+            else return -1.0;
+        }
+
+        public static double[] Calc6(double[] numbers)
+        {
+            int length = numbers.GetLength(0);
+            List<Double> aboveAvgList = new List<double>();
+
+            if (length > 0)
+            {
+                double avg = Calc5(numbers);
+
+                foreach (double number in numbers)
+                {
+                    if (number > avg)
+                    aboveAvgList.Add(number);
+                }
+
+                return aboveAvgList.ToArray();
+            }
+
+            return aboveAvgList.ToArray();
+        }
+    }
+}
---
+++ kpavlak1730EX3cUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("kpavlak1730EX3cUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("kpavlak1730EX3cUnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7ebe48b7-a431-4f25-98c1-810027f95d54")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
